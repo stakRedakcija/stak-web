e5a477f966ba0f922cf028f0a82e4e20258ae064
Goran Alković <goranalkovic@users.noreply.github.com>
2023-02-08 00:01:20 +0100

    add FeaturedPerson blog component

---
+++ src/lib/article-components/FeaturedPerson.svelte
@@ -0,0 +1,16 @@
+<script>
+	export let name;
+	export let description;
+	export let imageUrl;
+
+	let className;
+	export { className as class };
+</script>
+
+<div class="flex items-center gap-6 {className}">
+	<img class="m-0 p-0 w-40 aspect-square object-cover rounded-xl dark:brightness-75 dark:saturate-[75%] border-8 border-white dark:border-zinc-200 shadow -rotate-3" src={imageUrl} alt={name} />
+	<div class="flex flex-col gap-0.5 leading-none">
+		<span class="text-xl wide-title text-amber-700 dark:text-yellow-100 font-normal">{name}</span>
+		<span class="font-handwriting text-stone-500 dark:text-zinc-400">{description}</span>
+	</div>
+</div>
