66e559f48c671beb41155ef011f8b3c282e27221
Goran Alković <goranalkovic@users.noreply.github.com>
2023-03-21 22:56:27 +0100

    add stak 26

--- src/routes/casopisi/+page.svelte
+++ src/routes/casopisi/+page.svelte
@@ -2,6 +2,8 @@
 	import magazines from './casopisi.json';
 
 	const { title: firstMagTitle, time: firstMagTime, summary: firstMagSummary, coverImageUrl: firstMagCoverImageUrl, url: firstMagUrl } = magazines[0];
+
+	const firstUrlParams = firstMagUrl?.startsWith('https://') ? 'target="_blank" rel="noreferrer"' : '';
 </script>
 
 <svelte:head>
@@ -12,7 +14,7 @@
 
 <div class="w-full flex max-xl:flex-col gap-40 p-10 md:p-20">
 	<div class="w-80 md:w-auto md:max-w-2xl xl:w-80 shrink-0 xl:sticky xl:top-20 xl:self-start xl:ml-2 flex flex-col md:flex-row xl:flex-col max-xl:mx-auto gap-0 md:gap-8 xl:gap-0">
-		<img class="rounded-2xl shadow-2xl shadow-zinc-700" src={firstMagCoverImageUrl} alt="Naslovnica {firstMagTitle}" />
+		<img class="rounded-2xl shadow-2xl shadow-zinc-700 max-w-xs" src={firstMagCoverImageUrl} alt="Naslovnica {firstMagTitle}" />
 
 		<div class="mt-6 flex flex-col items-start">
 			<h2 class="text-4xl font-headline font-bold tracking-tighter text-emerald-400">{@html firstMagTitle}</h2>
@@ -20,13 +22,14 @@
 
 			<p class="mt-4">{@html firstMagSummary}</p>
 
-			<a class="px-4 py-1 rounded-md mt-8 flex shadow-md text-lg font-headline font-semibold bg-black hover:bg-emerald-400 text-emerald-50 hover:text-black hover:shadow-lg shadow-black/50 hover:shadow-emerald-900 transition" href={firstMagUrl} target="_blank" rel="noreferrer">Prolistaj </a>
+			<a class="px-4 py-1 rounded-md mt-8 flex shadow-md text-lg font-headline font-semibold bg-black hover:bg-emerald-400 text-emerald-50 hover:text-black hover:shadow-lg shadow-black/50 hover:shadow-emerald-900 transition" href={firstMagUrl} {firstUrlParams}>Prolistaj </a>
 		</div>
 	</div>
 
 	<div class="flex flex-wrap justify-center xl:justify-start gap-10 max-xl:mx-auto">
 		{#each magazines.slice(1) as { title, time, coverImageUrl, url }, i (i)}
-			<a class="flex flex-col group h-96 max-xl:items-center" href={url} target="_blank" rel="noreferrer">
+			{@const isExternal = url?.startsWith('https://')}
+			<a class="flex flex-col group h-96 max-xl:items-center" href={url} target="{isExternal ? '_blank' : null}" rel={isExternal ? 'noreferrer' : null}>
 				<img class="rounded-lg shadow shadow-black group-hover:shadow-emerald-800 group-hover:shadow-lg transition w-40 lg:w-56" src={coverImageUrl} alt="Naslovnica {title}" />
 
 				<h2 class="text-xl tracking-tighter mt-4 font-headline font-semibold text-white group-hover:text-emerald-200 transition">{@html title}</h2>
---
+++ src/routes/casopisi/26/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+	import { ArrowLeft } from "@inqling/svelte-icons/heroicon-24-solid";
+	import { DocumentArrowDown } from "@inqling/svelte-icons/heroicon-24-outline";
+</script>
+
+<a style="font-stretch: 150%; font-weight: 280;" class="text-base flex items-center gap-2 mt-10 mb-6 mx-10 md:mx-20" href="/casopisi"><ArrowLeft class="text-emerald-400 w-5 h-5" />Časopisi</a>
+
+<h1 class="wide-title text-9xl font-headline tracking-tighter m-10 md:m-20 mb-0 md:mb-0">Stak 26</h1>
+
+<div class="w-full flex max-xl:flex-col gap-10 p-10 md:p-20">
+	<iframe class="w-full aspect-[210/297] lg:aspect-video rounded-xl scrollbar-hide" title="Stak 26" src="/casopisi/stak-26.pdf" frameborder="0"></iframe>
+
+	<a class="mx-auto px-4 py-1 rounded-md flex items-center gap-2 shadow-md text-lg font-headline font-semibold bg-black hover:bg-emerald-400 text-emerald-50 hover:text-black hover:shadow-lg shadow-black/50 hover:shadow-emerald-900 transition" href='/casopisi/stak-26.pdf' target="_blank">
+		<DocumentArrowDown class="w-6 h-6" />
+		Preuzmi PDF
+	</a>
+</div>
