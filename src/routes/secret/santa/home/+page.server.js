48684ccb8baa882be1d17b42d6d5ec6df02151cd
Goran Alković <goranalkovic@users.noreply.github.com>
2023-11-13 22:09:34 +0100

    add debug logs

--- src/routes/secret/santa/home/+page.server.js
+++ src/routes/secret/santa/home/+page.server.js
@@ -1,42 +1,44 @@
-import {redirect} from '@sveltejs/kit'
-import {PUBLIC_SANTA_START, PUBLIC_SANTA_END} from '$env/static/public'
+import { redirect } from '@sveltejs/kit'
+import { PUBLIC_SANTA_START, PUBLIC_SANTA_END } from '$env/static/public'
 import members from '../../../clanovi.json'
-import {isDateBetween} from '$lib/utils/index.js'
+import { isDateBetween } from '$lib/utils/index.js'
 
 const validEmails = members.map((it) => it.email).filter((it) => it)
 const santaStartDate = new Date(PUBLIC_SANTA_START)
 const santaEndDate = new Date(PUBLIC_SANTA_END)
 
-export const load = async ({url, locals: {supabase, getSession}}) => {
-    const session = await getSession()
-    const response = {
-        error: '',
-        event: {
-            startDate: santaStartDate,
-            endDate: santaEndDate,
-            isDrawingTime: isDateBetween(santaStartDate, santaEndDate)
-        },
-        user: {},
-        elf: {}
-    }
+export const load = async ({ url, locals: { supabase, getSession } }) => {
+	console.log(url);
 
-    if (isSessionAvailable(session)) {
-        if (!isStakMember(session.user.email)) {
-            await deleteUser(supabase, session.user)
-            response.error = 'You\'re not on Santa\'s list... ):'
-            return response
-        }
-    } else {
-        await signInWithDiscord(supabase, url)
-    }
+	const session = await getSession()
+	const response = {
+		error: '',
+		event: {
+			startDate: santaStartDate,
+			endDate: santaEndDate,
+			isDrawingTime: isDateBetween(santaStartDate, santaEndDate)
+		},
+		user: {},
+		elf: {}
+	}
 
-    response.user = extractUserInfo(session.user)
+	if (isSessionAvailable(session)) {
+		if (!isStakMember(session.user.email)) {
+			await deleteUser(supabase, session.user)
+			response.error = 'You\'re not on Santa\'s list... ):'
+			return response
+		}
+	} else {
+		await signInWithDiscord(supabase, url)
+	}
 
-    if (response.event.isDrawingTime) {
-        response.elf = await getElfForUser(supabase, session.user.id)
-    }
+	response.user = extractUserInfo(session.user)
 
-    return response
+	if (response.event.isDrawingTime) {
+		response.elf = await getElfForUser(supabase, session.user.id)
+	}
+
+	return response
 }
 
 const isSessionAvailable = (session) => session !== null
@@ -44,44 +46,46 @@ const isSessionAvailable = (session) => session !== null
 const isStakMember = (email) => validEmails.includes(email)
 
 const deleteUser = async (supabase, user) => {
-    await supabase.auth.signOut()
-    await supabase.auth.admin.deleteUser(user.id)
+	await supabase.auth.signOut()
+	await supabase.auth.admin.deleteUser(user.id)
 }
 
 const signInWithDiscord = async (supabase, url) => {
-    const {data} = await supabase.auth.signInWithOAuth({
-        provider: 'discord',
-        options: {
-            scopes: 'identify',
-            redirectTo: `${url.protocol}//${url.host}/api/auth/callback?next=/secret/santa/home`,
-        }
-    })
+	console.log(url);
 
-    throw redirect(303, data.url)
+	const { data } = await supabase.auth.signInWithOAuth({
+		provider: 'discord',
+		options: {
+			scopes: 'identify',
+			redirectTo: `${url.protocol}//${url.host}/api/auth/callback?next=/secret/santa/home`,
+		}
+	})
+
+	throw redirect(303, data.url)
 }
 
 const extractUserInfo = (user) => {
-    const {email, user_metadata, raw_user_meta_data} = user
-    const {
-        picture: src,
-        full_name: username,
-        custom_claims: {global_name: fullName}
-    } = user_metadata ?? raw_user_meta_data
+	const { email, user_metadata, raw_user_meta_data } = user
+	const {
+		picture: src,
+		full_name: username,
+		custom_claims: { global_name: fullName }
+	} = user_metadata ?? raw_user_meta_data
 
-    return {email, src, fullName, username}
+	return { email, src, fullName, username }
 }
 
 const getElfForUser = async (supabase, userId) => {
-    const {data, error} = await supabase.rpc('get_elf', {userid: userId})
-    if (error) return {}
+	const { data, error } = await supabase.rpc('get_elf', { userid: userId })
+	if (error) return {}
 
-    const {elf_id, shown: isShown} = data
-    if (!elf_id) return {}
+	const { elf_id, shown: isShown } = data
+	if (!elf_id) return {}
 
-    const elf = await supabase.rpc('get_user', {userid: elf_id})
+	const elf = await supabase.rpc('get_user', { userid: elf_id })
 
-    return {
-        ...extractUserInfo(elf.data),
-        isShown
-    }
+	return {
+		...extractUserInfo(elf.data),
+		isShown
+	}
 }
