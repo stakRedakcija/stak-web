c256ee37f746da023e7b75d6301db49ef9c630ed
Tin <ttomasic20@foi.hr>
2023-11-16 13:29:44 +0100

    TT - added spaced comment rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -14,6 +14,7 @@ module.exports = {
 	},
 	rules: {
 		'semi': ['error', 'never'],
-		'comma-dangle': ['error', 'never']
+		'comma-dangle': ['error', 'never'],
+		'spaced-comment': ['error', 'always']
 	}
 }
