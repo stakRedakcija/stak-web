7e997805acb92e9579a2e69115f2f862cc5489fb
Tin <tin.tomasic74@gmail.com>
2023-12-21 18:46:31 +0100

    TT - added SSR Snow component

---
+++ src/lib/components/Snow.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { onMount } from 'svelte'
+	import { loadSlim } from 'tsparticles-slim'
+	import snowConfig from './snowConfig.json'
+
+	let ParticlesComponent
+
+	onMount(async () => {
+		const module = await import('svelte-particles')
+		ParticlesComponent = module.default
+	})
+
+	let particlesInit = async (engine) => await loadSlim(engine)
+</script>
+
+<svelte:component
+        this={ParticlesComponent}
+        id="snow-particles"
+        options={snowConfig}
+        {particlesInit}
+        class="z-0 pointer-events-none"
+/>
+
+<style global>
+    #snow-particles canvas {
+        pointer-events: none !important;
+    }
+</style>
