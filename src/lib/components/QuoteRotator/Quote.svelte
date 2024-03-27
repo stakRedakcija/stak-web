b70f66f628338d8d53b7548a475e9ba83050fc98
Tin <tin.tomasic74@gmail.com>
2024-03-27 17:09:37 +0100

    TT - add Quote components

---
+++ src/lib/components/QuoteRotator/Quote.svelte
@@ -0,0 +1,13 @@
+<script>
+	import {slide} from 'svelte/transition'
+
+	export let title
+	export let quote
+	export let author
+</script>
+
+<div class="block w-[70%] text-justify font-light text-base" transition:slide>
+    <h1 class="font-light mb-2">{title}</h1>
+    <q class="text-white font-sans">{@html quote}</q>
+    <p class="text-right my-4 italic font-handwriting">&mdash;{author}</p>
+</div>
