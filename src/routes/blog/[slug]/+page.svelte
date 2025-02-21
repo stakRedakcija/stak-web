ef624cab5786dc3bcf216f4b1ffa3a77112ee90f
David Slavik <slavikdavid@protonmail.com>
2025-02-22 00:38:37 +0100

    Fix og meta tag extraction logic. Add og images for each individual page. Add excerpt on articles which don't have text content

--- src/routes/blog/[slug]/+page.svelte
+++ src/routes/blog/[slug]/+page.svelte
@@ -12,8 +12,6 @@
     <meta property="og:title" content={title.replace(/(<([^>]+)>)/gi, '')} />
     <meta property="og:description" content={data.ogDescription} />
     <meta property="og:image" content={data.image} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
     <meta property="og:url" content={`https://stak.foi.hr/blog/${data.slug}`} />
     <meta property="og:type" content="article" />
 </svelte:head>
